c9aeb6777273e40dac8cf4b2baed9a05db48a76b
Unknown <edelacruz@teravisiontech.com>
2017-11-06 02:21:52 -0400

    Almost Done

--- TwitterLBTA/Controller/HomeDatasourceController.swift
+++ TwitterLBTA/Controller/HomeDatasourceController.swift
@@ -20,13 +20,18 @@ class HomeDatasourceController: DatasourceController
         self.datasource = homeDatasource
     }
     
+    override func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize
+    {
+        return CGSize(width: view.frame.width, height: 150.0)
+    }
+    
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, referenceSizeForHeaderInSection section: Int) -> CGSize
     {
-        return CGSize(width: view.frame.width, height: 50)
+        return CGSize(width: view.frame.width, height: 50.0)
     }
     
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, referenceSizeForFooterInSection section: Int) -> CGSize
     {
-        return CGSize(width: view.frame.width, height: 100)
+        return CGSize(width: view.frame.width, height: 100.0)
     }
 }
--- TwitterLBTA/View/Cells.swift
+++ TwitterLBTA/View/Cells.swift
@@ -37,28 +37,82 @@ class UserHeader: DatasourceCell
 class UserCell: DatasourceCell
 {
     override var datasourceItem: Any?
-        {
+    {
         didSet
         {
-            nameLabel.text = datasourceItem as? String
+//            nameLabel.text = datasourceItem as? String
         }
     }
+    
+    let profileImageView : UIImageView =
+    {
+        let imageView = UIImageView()
+        imageView.image = #imageLiteral(resourceName: "ProfilePicture")
+        imageView.layer.cornerRadius = 5
+        imageView.clipsToBounds = true
+        return imageView
+    }()
     
     let nameLabel : UILabel =
     {
         let label = UILabel()
-        label.text = "TEST TEST TEST"
+        label.text = "Eduardo De La Cruz"
+        label.font = UIFont.boldSystemFont(ofSize: 16.0)
         
         return label
+    }()
+    
+    let userNameLabel : UILabel =
+    {
+        let label = UILabel()
+        label.text = "@gualdillo"
+        label.font = UIFont.systemFont(ofSize: 14.0)
+        label.textColor = UIColor(r: 130.0, g: 130.0, b: 130.0)
+        
+        return label
+    }()
+    
+    let bioTextView : UITextView =
+    {
+        let textView = UITextView()
+        textView.text = "iPhone, iPad, iOS Programming Community. Join us to learn Swift, Objective-C and build iOS apps!"
+        textView.font = UIFont.systemFont(ofSize: 15)
+        
+        return textView
+    }()
+    
+    let followButton : UIButton =
+    {
+        let twitterBlue = UIColor(r: 61.0, g: 167.0, b: 244.0)
+        let button = UIButton()
+        button.layer.cornerRadius = 5.0
+        button.layer.borderColor = twitterBlue.cgColor
+        button.layer.borderWidth = 1
+        button.setTitle("Follow", for: .normal)
+        button.titleLabel?.font = UIFont.boldSystemFont(ofSize: 14)
+        button.setTitleColor(twitterBlue, for: .normal)
+        
+        return button
     }()
     
     override func setupViews()
     {
         super.setupViews()
         
-        backgroundColor = .yellow
-        
+        addSubview(profileImageView)
         addSubview(nameLabel)
-        nameLabel.anchor(topAnchor, left: leftAnchor, bottom: bottomAnchor, right: rightAnchor, topConstant: 0.0, leftConstant: 0.0, bottomConstant: 0.0, rightConstant: 0.0, widthConstant: 0.0, heightConstant: 0.0)
+        addSubview(userNameLabel)
+        addSubview(bioTextView)
+        addSubview(followButton)
+        
+        profileImageView.anchor(self.topAnchor, left: self.leftAnchor, bottom: nil, right: nil, topConstant: 12.0, leftConstant: 12.0, bottomConstant: 0.0, rightConstant: 0.0, widthConstant: 50.0, heightConstant: 50.0)
+        
+        nameLabel.anchor(profileImageView.topAnchor, left: profileImageView.rightAnchor, bottom: nil, right: followButton.leftAnchor, topConstant: 0.0, leftConstant: 8.0, bottomConstant: 0.0, rightConstant: 12.0, widthConstant: 0.0, heightConstant: 20.0)
+        
+        userNameLabel.anchor(nameLabel.bottomAnchor, left: nameLabel.leftAnchor, bottom: nil, right: nameLabel.rightAnchor, topConstant: 0.0, leftConstant: 0.0, bottomConstant: 0.0, rightConstant: 0.0, widthConstant: 0.0, heightConstant: 24.0)
+        
+        bioTextView.anchor(userNameLabel.bottomAnchor, left: userNameLabel.leftAnchor, bottom: self.bottomAnchor, right: self.rightAnchor, topConstant: -4.0, leftConstant: -4.0, bottomConstant: 0.0, rightConstant: 0.0, widthConstant: 0.0, heightConstant: 0.0)
+        
+        followButton.anchor(self.topAnchor, left: nil, bottom: nil, right: self.rightAnchor, topConstant: 12.0, leftConstant: 0.0, bottomConstant: 0.0, rightConstant: 12.0, widthConstant: 120, heightConstant: 34)
     }
 }
